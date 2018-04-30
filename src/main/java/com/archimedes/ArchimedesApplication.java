9e6c86906b95df837d5246a27023cb4bc55f1fb4
tanxiazhe <tanxiazhe@163.com>
2018-04-30 21:42:58 +0800

    spring-boot session with redis

--- src/main/java/com/archimedes/ArchimedesApplication.java
+++ src/main/java/com/archimedes/ArchimedesApplication.java
@@ -3,9 +3,11 @@ package com.archimedes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableRedisHttpSession
 @EnableSwagger2
 @SpringBootApplication
 @ComponentScan(basePackages={"com.archimedes.*"})
